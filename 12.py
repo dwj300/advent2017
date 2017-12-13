7a190b01e7d1dd5da16c62fbdeb302502c920f25
Douglas Jordan <dojordan@microsoft.com>
2017-12-12 17:30:44 -0800

    Day12

---
+++ 12.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+sample = """0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5"""
+
+def reachable(graph, k, visited):
+    if k == 0:
+        return True
+    if 0 in graph[k]:
+        return True
+    if k in visited:
+        return False
+    return any(map(lambda x: reachable(graph, x, visited + [k]), graph[k]))
+
+def build_graph(in_txt): 
+    graph = {}
+    for l in in_txt.split('\n'):
+        parts = l.split(' ')
+        graph[int(parts[0])] = list(map(lambda x: int(x.strip(',')), parts[2:]))
+    return graph
+ 
+def num_connections(in_txt):
+    graph = build_graph(in_txt)
+    count = 0
+    for k in graph.keys():
+        if reachable(graph, k, []):
+            count += 1
+    return count
+
+def docolor(colors, k, color, graph):
+    if k in colors:
+        return
+    colors[k] = color
+    for newk in graph[k]:
+        docolor(colors, newk, color, graph)
+    
+def num_groups(in_txt):
+    graph = build_graph(in_txt)
+    colors = {}
+    #for k in graph.keys():
+    #    colors[k] = k
+    for k in graph.keys():
+        docolor(colors, k, k, graph)
+    return len(set(list(colors.values())))
+
+if __name__ == "__main__":
+    assert(num_connections(sample) == 6)
+    assert(num_groups(sample) == 2)
+    with open("12_in.txt") as f:
+        text = f.read().strip()
+        print("Part 1: {0}".format(num_connections(text)))
+        print("Part 2: {0}".format(num_groups(text)))

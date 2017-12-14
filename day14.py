c3149fcbe350b53f14069092a817d41abe5c1fb2
Douglas Jordan <dojordan@microsoft.com>
2017-12-14 15:29:01 -0800

    reducing day 14

--- day14.py
+++ day14.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-from functools import reduce
 from itertools import product
-from day10 import *
+from day10 import strhash
 
 def part1(in_txt):
     return sum(map(lambda i: calc("{0}-{1}".format(in_txt, i)).count('1'), range(128)))
@@ -10,19 +9,15 @@ def calc(knot):
     return "".join(list(map(lambda c: "{:04b}".format(int(c,16)), strhash(knot))))
 
 def part2(in_txt):
-    a = [[int(j) for j in calc("{0}-{1}".format(in_txt, i))] for i in range(128)]
     colors = {}
-    for c, (i,j) in enumerate(product(range(128), range(128))):
-        color(a, i, j, colors, c)
-    # this doesn't work correctly?
-    # map(lambda t: color(a, t[1][0], t[1][1], colors, t[0]), enumerate(product(range(128), range(128)))) 
+    a = [[int(j) for j in calc("{0}-{1}".format(in_txt, i))] for i in range(128)]
+    list(map(lambda t: color(a, t[1][0], t[1][1], colors, t[0]), enumerate(product(range(128), range(128)))))
     return len(set(colors.values()))
 
 def color(a, x, y, colors, c):
     if x < 0 or x >= 128 or y < 0 or y >= 128 or a[x][y] == 0 or (x,y) in colors: return
     colors[(x,y)] = c
-    for (i,j) in [(1,0),(-1,0),(0,1),(0,-1)]:
-        color(a,x+i,y+j,colors,c),
+    list(map(lambda t: color(a,x+t[0],y+t[1],colors,c), [(1,0),(-1,0),(0,1),(0,-1)])) 
 
 if __name__ == "__main__":
     sample = "flqrgnkx"

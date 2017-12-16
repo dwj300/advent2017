31b17e583e4de6abaaa5a1105d11606573d1c5dc
Douglas Jordan <dwj300@gmail.com>
2017-12-15 19:40:43 -0800

    reducing loc

--- day13.py
+++ day13.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import copy
+from itertools import count
 def severity(text):
     weights = _parse_input(text)
     penalty = 0
@@ -11,25 +12,6 @@ def severity(text):
         cur += 1
         _dostep(weights) 
     return penalty
-
-def delay(text):
-    weights = _parse_input(text)
-    cur = 0 
-    dl = 0
-    m = max(weights.keys())
-    while True:
-        start = copy.deepcopy(weights) 
-        while cur <= m:
-            if cur in weights and weights[cur][1] == 0:
-                dl += 1
-                weights = start
-                _dostep(weights)
-                cur = 0
-                break
-            cur += 1
-            _dostep(weights)
-        if cur > m:
-            return dl
 
 def _dostep(weights, num=1):
     for _ in range(num):
@@ -51,14 +33,30 @@ def _parse_input(text):
         weights[int(parts[0])] = [int(parts[1].strip()), 0, 1]
     return weights
 
+def caught(weights, delay):
+    for t in weights.items():
+        if (delay+t[0]) % ((t[1][0]-1)*2) == 0:
+            return True
+    return False
+
+def delay2(text):
+    weights = _parse_input(text)
+    #return any(map(lambda x: caught(weights, x), next(d for d in count())))
+    i = 0
+    while True:
+        if caught(weights, i):
+            i += 1
+        else:
+            return i
+
 if __name__ == "__main__":
     sample = """0: 3
 1: 2
 4: 4
 6: 4"""
     assert(severity(sample) == 24)
-    assert(delay(sample) == 10)
+    assert(delay2(sample) == 10)
     with open("13_in.txt") as f:
         txt = f.read().strip()
         print("Part 1: {0}".format(severity(txt)))
-        print("Part 2: {0}".format(delay(txt)))
+        print("Part 2: {0}".format(delay2(txt)))
